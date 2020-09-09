77c2d3992dd536091433fc9376a27ec2ef23f377
Moth Doctor <krzysiek.justynski@gmail.com>
2020-09-09 19:47:05 +0200

    styling nodes
    Former-commit-id: 713f18fd5f899c662a2b1fef5b684f1cefbcb6d8

--- Source/FlowEditor/Private/FlowEditorSettings.cpp
+++ Source/FlowEditor/Private/FlowEditorSettings.cpp
@@ -3,21 +3,21 @@
 UFlowEditorSettings::UFlowEditorSettings(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 	, bShowDefaultPinNames(false)
-	, NodeDescriptionBackground(FLinearColor(0.12f, 0.12f, 0.12f, 1.0f))
+	, NodeDescriptionBackground(FLinearColor(0.0625f, 0.0625f, 0.0625f, 1.0f))
 	, NodeStatusBackground(FLinearColor(0.12f, 0.12f, 0.12f, 1.0f))
 	, NodePreloadedBackground(FLinearColor(0.12f, 0.12f, 0.12f, 1.0f))
 	, RecentWireDuration(3.0f)
 	, RecentWireColor(FLinearColor(1.0f, 0.05f, 0.0f, 1.0f))
 	, RecentWireThickness(6.0f)
-	, RecordedWireColor(FLinearColor(0.6f, 0.39f, 0.18f, 1.0f))
+	, RecordedWireColor(FLinearColor(0.432f, 0.258f, 0.096f, 1.0f))
 	, RecordedWireThickness(3.5f)
-	, InactiveWireColor(FLinearColor(0.5f, 0.5f, 0.5f, 1.0f))
+	, InactiveWireColor(FLinearColor(0.364f, 0.364f, 0.364f, 1.0f))
 	, InactiveWireThickness(1.5f)
 {
-	NodeTitleColors.Emplace(EFlowNodeStyle::Condition, FLinearColor(1.0f, 0.895f, 0.016f, 1.0f));
-	NodeTitleColors.Emplace(EFlowNodeStyle::Default, FLinearColor(0.196f, 0.394f, 0.661f, 1.0f));
-	NodeTitleColors.Emplace(EFlowNodeStyle::InOut, FLinearColor(0.468f,0.014f,0.040f,1.0f));
-	NodeTitleColors.Emplace(EFlowNodeStyle::Latent, FLinearColor(0.398f, 0.0403f, 0.806f, 1.0f));
+	NodeTitleColors.Emplace(EFlowNodeStyle::Condition, FLinearColor(1.0f, 0.62f, 0.016f, 1.0f));
+	NodeTitleColors.Emplace(EFlowNodeStyle::Default, FLinearColor(-0.728f, 0.581f, 1.0f, 1.0f));
+	NodeTitleColors.Emplace(EFlowNodeStyle::InOut, FLinearColor(1.0f,0.0f,0.008f,1.0f));
+	NodeTitleColors.Emplace(EFlowNodeStyle::Latent, FLinearColor(0.0f, 0.770f, 0.375f, 1.0f));
 	NodeTitleColors.Emplace(EFlowNodeStyle::Logic, FLinearColor(1.0f, 1.0f, 1.0f, 1.0f));
-	NodeTitleColors.Emplace(EFlowNodeStyle::SubGraph, FLinearColor(0.588f,0.337f,0.058f,1.0f));
+	NodeTitleColors.Emplace(EFlowNodeStyle::SubGraph, FLinearColor(1.0f,0.128f,0.0f,1.0f));
 }
