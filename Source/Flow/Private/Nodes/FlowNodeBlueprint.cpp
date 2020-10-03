e16e5cdfcebbea53e2541b9632830595df111d2f
Moth Doctor <krzysiek.justynski@gmail.com>
2020-10-03 19:15:39 +0200

    disable creating new event graphs and delegates in Flow Node blueprints
    Former-commit-id: 37a3073d6bea1cb09e79af805ea987e0c982d5b3

--- Source/Flow/Private/Nodes/FlowNodeBlueprint.cpp
+++ Source/Flow/Private/Nodes/FlowNodeBlueprint.cpp
@@ -4,21 +4,3 @@ UFlowNodeBlueprint::UFlowNodeBlueprint(const FObjectInitializer& ObjectInitializ
 	: Super(ObjectInitializer)
 {
 }
-
-#if WITH_EDITOR
-UFlowNodeBlueprint* UFlowNodeBlueprint::FindRootFlowNodeBlueprint(UFlowNodeBlueprint* DerivedBlueprint)
-{
-	UFlowNodeBlueprint* ParentBP = nullptr;
-
-	// Determine if there is a Flow Node blueprint in the ancestry of this class
-	for (UClass* ParentClass = DerivedBlueprint->ParentClass; ParentClass != UObject::StaticClass(); ParentClass = ParentClass->GetSuperClass())
-	{
-		if (UFlowNodeBlueprint* TestBP = Cast<UFlowNodeBlueprint>(ParentClass->ClassGeneratedBy))
-		{
-			ParentBP = TestBP;
-		}
-	}
-
-	return ParentBP;
-}
-#endif
