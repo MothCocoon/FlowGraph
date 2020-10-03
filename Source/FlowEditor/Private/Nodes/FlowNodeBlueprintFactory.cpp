05d5b7961029b5eb83d4905d0f6b201ac18ae674
Moth Doctor <krzysiek.justynski@gmail.com>
2020-10-03 19:14:21 +0200

    added Class Picker while creating Flow Node blueprint

--- Source/FlowEditor/Private/Nodes/FlowNodeBlueprintFactory.cpp
+++ Source/FlowEditor/Private/Nodes/FlowNodeBlueprintFactory.cpp
@@ -3,7 +3,225 @@
 #include "Nodes/FlowNode.h"
 #include "Nodes/FlowNodeBlueprint.h"
 
+#include "BlueprintEditorSettings.h"
+#include "ClassViewerFilter.h"
+#include "ClassViewerModule.h"
 #include "Kismet2/KismetEditorUtilities.h"
+#include "SlateOptMacros.h"
+#include "Widgets/Layout/SUniformGridPanel.h"
+
+#define LOCTEXT_NAMESPACE "FlowNodeBlueprintFactory"
+
+// ------------------------------------------------------------------------------
+// Dialog to configure creation properties
+// ------------------------------------------------------------------------------
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+class SFlowNodeBlueprintCreateDialog : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SFlowNodeBlueprintCreateDialog){}
+
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs)
+	{
+		bOkClicked = false;
+		ParentClass = UFlowNode::StaticClass();
+
+		ChildSlot
+			[
+				SNew(SBorder)
+				.Visibility(EVisibility::Visible)
+				.BorderImage(FEditorStyle::GetBrush("Menu.Background"))
+				[
+					SNew(SBox)
+					.Visibility(EVisibility::Visible)
+					.WidthOverride(500.0f)
+					[
+						SNew(SVerticalBox)
+						+ SVerticalBox::Slot()
+						.FillHeight(1)
+						[
+							SNew(SBorder)
+							.BorderImage(FEditorStyle::GetBrush("ToolPanel.GroupBorder"))
+							.Content()
+							[
+								SAssignNew(ParentClassContainer, SVerticalBox)
+							]
+						]
+
+						// Ok/Cancel buttons
+						+ SVerticalBox::Slot()
+						.AutoHeight()
+						.HAlign(HAlign_Right)
+						.VAlign(VAlign_Bottom)
+						.Padding(8)
+						[
+							SNew(SUniformGridPanel)
+							.SlotPadding(FEditorStyle::GetMargin("StandardDialog.SlotPadding"))
+							.MinDesiredSlotWidth(FEditorStyle::GetFloat("StandardDialog.MinDesiredSlotWidth"))
+							.MinDesiredSlotHeight(FEditorStyle::GetFloat("StandardDialog.MinDesiredSlotHeight"))
+
+							+ SUniformGridPanel::Slot(0, 0)
+							[
+								SNew(SButton)
+								.HAlign(HAlign_Center)
+								.ContentPadding(FEditorStyle::GetMargin("StandardDialog.ContentPadding"))
+								.OnClicked(this, &SFlowNodeBlueprintCreateDialog::OkClicked)
+								.Text(LOCTEXT("CreateFlowNodeBlueprintOk", "OK"))
+							]
+
+							+ SUniformGridPanel::Slot(1, 0)
+							[
+								SNew(SButton)
+								.HAlign(HAlign_Center)
+								.ContentPadding(FEditorStyle::GetMargin("StandardDialog.ContentPadding"))
+								.OnClicked(this, &SFlowNodeBlueprintCreateDialog::CancelClicked)
+								.Text(LOCTEXT("CreateFlowNodeBlueprintCancel", "Cancel"))
+							]
+						]
+					]
+				]
+			];
+
+			MakeParentClassPicker();
+		}
+
+	/** Sets properties for the supplied FlowNodeBlueprintFactory */
+	bool ConfigureProperties(const TWeakObjectPtr<UFlowNodeBlueprintFactory> InFlowNodeBlueprintFactory)
+	{
+		FlowNodeBlueprintFactory = InFlowNodeBlueprintFactory;
+
+		const TSharedRef<SWindow> Window = SNew(SWindow)
+			.Title(LOCTEXT("CreateFlowNodeBlueprintOptions", "Pick Parent Class"))
+			.ClientSize(FVector2D(400, 700))
+			.SupportsMinimize(false).SupportsMaximize(false)
+			[
+				AsShared()
+			];
+
+		PickerWindow = Window;
+		GEditor->EditorAddModalWindow(Window);
+		
+		FlowNodeBlueprintFactory.Reset();
+		return bOkClicked;
+	}
+
+private:
+	class FFlowNodeBlueprintParentFilter final : public IClassViewerFilter
+	{
+	public:
+		/** All children of these classes will be included unless filtered out by another setting. */
+		TSet<const UClass*> AllowedChildrenOfClasses;
+
+		FFlowNodeBlueprintParentFilter() {}
+
+		virtual bool IsClassAllowed(const FClassViewerInitializationOptions& InInitOptions, const UClass* InClass, TSharedRef< FClassViewerFilterFuncs > InFilterFuncs) override
+		{
+			// If it appears on the allowed child-of classes list (or there is nothing on that list)
+			return InFilterFuncs->IfInChildOfClassesSet(AllowedChildrenOfClasses, InClass) != EFilterReturn::Failed;
+		}
+
+		virtual bool IsUnloadedClassAllowed(const FClassViewerInitializationOptions& InInitOptions, const TSharedRef< const IUnloadedBlueprintData > InUnloadedClassData, TSharedRef< FClassViewerFilterFuncs > InFilterFuncs) override
+		{
+			// If it appears on the allowed child-of classes list (or there is nothing on that list)
+			return InFilterFuncs->IfInChildOfClassesSet(AllowedChildrenOfClasses, InUnloadedClassData) != EFilterReturn::Failed;
+		}
+	};
+
+	/** Creates the combo menu for the parent class */
+	void MakeParentClassPicker()
+	{
+		// Load the Class Viewer module to display a class picker
+		FClassViewerModule& ClassViewerModule = FModuleManager::LoadModuleChecked<FClassViewerModule>("ClassViewer");
+
+		FClassViewerInitializationOptions Options;
+		Options.Mode = EClassViewerMode::ClassPicker;
+		Options.DisplayMode = EClassViewerDisplayMode::TreeView;
+		Options.bIsBlueprintBaseOnly = true;
+
+		TSharedPtr<FFlowNodeBlueprintParentFilter> Filter = MakeShareable(new FFlowNodeBlueprintParentFilter);
+
+		// All child child classes of UFlowNode are valid
+		Filter->AllowedChildrenOfClasses.Add(UFlowNode::StaticClass());
+		Options.ClassFilter = Filter;
+
+		ParentClassContainer->ClearChildren();
+		ParentClassContainer->AddSlot()
+			[
+				ClassViewerModule.CreateClassViewer(Options, FOnClassPicked::CreateSP(this, &SFlowNodeBlueprintCreateDialog::OnClassPicked))
+			];
+	}
+
+	/** Handler for when a parent class is selected */
+	void OnClassPicked(UClass* ChosenClass)
+	{
+		ParentClass = ChosenClass;
+	}
+
+	/** Handler for when ok is clicked */
+	FReply OkClicked()
+	{
+		if (FlowNodeBlueprintFactory.IsValid())
+		{
+			FlowNodeBlueprintFactory->ParentClass = ParentClass.Get();
+		}
+
+		CloseDialog(true);
+
+		return FReply::Handled();
+	}
+
+	void CloseDialog(bool bWasPicked = false)
+	{
+		bOkClicked = bWasPicked;
+		if (PickerWindow.IsValid())
+		{
+			PickerWindow.Pin()->RequestDestroyWindow();
+		}
+	}
+
+	/** Handler for when cancel is clicked */
+	FReply CancelClicked()
+	{
+		CloseDialog();
+		return FReply::Handled();
+	}
+
+	virtual FReply OnKeyDown(const FGeometry& MyGeometry, const FKeyEvent& InKeyEvent) override
+	{
+		if (InKeyEvent.GetKey() == EKeys::Escape)
+		{
+			CloseDialog();
+			return FReply::Handled();
+		}
+		return SWidget::OnKeyDown(MyGeometry, InKeyEvent);
+	}
+
+private:
+	/** The factory for which we are setting up properties */
+	TWeakObjectPtr<UFlowNodeBlueprintFactory> FlowNodeBlueprintFactory;
+
+	/** A pointer to the window that is asking the user to select a parent class */
+	TWeakPtr<SWindow> PickerWindow;
+
+	/** The container for the Parent Class picker */
+	TSharedPtr<SVerticalBox> ParentClassContainer;
+
+	/** The selected class */
+	TWeakObjectPtr<UClass> ParentClass;
+
+	/** True if Ok was clicked */
+	bool bOkClicked = false;
+};
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+/*------------------------------------------------------------------------------
+	UFlowNodeBlueprintFactory implementation
+------------------------------------------------------------------------------*/
 
 UFlowNodeBlueprintFactory::UFlowNodeBlueprintFactory(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -15,9 +233,37 @@ UFlowNodeBlueprintFactory::UFlowNodeBlueprintFactory(const FObjectInitializer& O
 	bEditAfterNew = true;
 }
 
+bool UFlowNodeBlueprintFactory::ConfigureProperties()
+{
+	TSharedRef<SFlowNodeBlueprintCreateDialog> Dialog = SNew(SFlowNodeBlueprintCreateDialog);
+	return Dialog->ConfigureProperties(this);
+}
+
 UObject* UFlowNodeBlueprintFactory::FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn, FName CallingContext)
 {
+	check(Class->IsChildOf(UFlowNodeBlueprint::StaticClass()));
+
+	if (ParentClass == nullptr || !FKismetEditorUtilities::CanCreateBlueprintOfClass(ParentClass) || !ParentClass->IsChildOf(UFlowNode::StaticClass()))
+	{
+		FFormatNamedArguments Args;
+		Args.Add(TEXT("ClassName"), ParentClass ? FText::FromString(ParentClass->GetName()) : LOCTEXT("Null", "(null)"));
+		FMessageDialog::Open(EAppMsgType::Ok, FText::Format(LOCTEXT("CannotCreateFlowNodeBlueprint", "Cannot create a Flow Node Blueprint based on the class '{ClassName}'."), Args));
+		return nullptr;
+	}
+
 	UFlowNodeBlueprint* NewBP = CastChecked<UFlowNodeBlueprint>(FKismetEditorUtilities::CreateBlueprint(ParentClass, InParent, Name, BPTYPE_Normal, UFlowNodeBlueprint::StaticClass(), UBlueprintGeneratedClass::StaticClass(), CallingContext));
+
+	if (NewBP && NewBP->UbergraphPages.Num() > 0)
+	{
+		UBlueprintEditorSettings* Settings = GetMutableDefault<UBlueprintEditorSettings>();
+		if(Settings && Settings->bSpawnDefaultBlueprintNodes)
+		{
+			int32 NodePositionY = 0;
+			FKismetEditorUtilities::AddDefaultEventNode(NewBP, NewBP->UbergraphPages[0], FName(TEXT("K2_ExecuteInput")), UFlowNode::StaticClass(), NodePositionY);
+			FKismetEditorUtilities::AddDefaultEventNode(NewBP, NewBP->UbergraphPages[0], FName(TEXT("K2_Cleanup")), UFlowNode::StaticClass(), NodePositionY);
+		}
+	}
+	
 	return NewBP;
 }
 
@@ -25,3 +271,5 @@ UObject* UFlowNodeBlueprintFactory::FactoryCreateNew(UClass* Class, UObject* InP
 {
 	return FactoryCreateNew(Class, InParent, Name, Flags, Context, Warn, NAME_None);
 }
+
+#undef LOCTEXT_NAMESPACE
