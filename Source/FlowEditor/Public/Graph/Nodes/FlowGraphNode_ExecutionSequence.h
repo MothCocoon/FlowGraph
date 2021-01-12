6242f90f821aeff21e52918febc88056e89ed748
Moth Doctor <krzysiek.justynski@gmail.com>
2021-01-12 13:18:39 +0100

    assigning the same icon as in blueprint counterpart

---
+++ Source/FlowEditor/Public/Graph/Nodes/FlowGraphNode_ExecutionSequence.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Graph/Nodes/FlowGraphNode.h"
+#include "FlowGraphNode_ExecutionSequence.generated.h"
+
+UCLASS()
+class UFlowGraphNode_ExecutionSequence final : public UFlowGraphNode
+{
+	GENERATED_UCLASS_BODY()
+
+	// UEdGraphNode
+	virtual FSlateIcon GetIconAndTint(FLinearColor& OutColor) const override;
+	// --
+};
