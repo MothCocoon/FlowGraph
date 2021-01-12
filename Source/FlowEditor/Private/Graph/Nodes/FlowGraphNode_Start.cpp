b0f17945cd9d502e157cef84a322aeec39a4e432
Moth Doctor <krzysiek.justynski@gmail.com>
2021-01-12 13:17:40 +0100

    rename Graph Node classes to match runtime classes

---
+++ Source/FlowEditor/Private/Graph/Nodes/FlowGraphNode_Start.cpp
@@ -0,0 +1,15 @@
+#include "Graph/Nodes/FlowGraphNode_Start.h"
+#include "Graph/Widgets/SFlowGraphNode_Start.h"
+
+#include "Nodes/Route/FlowNode_Start.h"
+
+UFlowGraphNode_Start::UFlowGraphNode_Start(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	AssignedNodeClasses = {UFlowNode_Start::StaticClass()};
+}
+
+TSharedPtr<SGraphNode> UFlowGraphNode_Start::CreateVisualWidget()
+{
+	return SNew(SFlowGraphNode_Start, this);
+}
