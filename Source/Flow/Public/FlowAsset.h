8b44c549d2d7b8545c52a374b7d776a502323a4c
sturcotte06 <sturcotte06@gmail.com>
2021-03-15 15:52:38 -0400

    Merge pull request #20
    * adds is data valid to flow asset to validate nodes
    
    * adds missing forward declare and include

--- Source/Flow/Public/FlowAsset.h
+++ Source/Flow/Public/FlowAsset.h
@@ -53,6 +53,7 @@ class FLOW_API UFlowAsset : public UObject
 	static void AddReferencedObjects(UObject* InThis, FReferenceCollector& Collector);
 	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
 	virtual void PostDuplicate(bool bDuplicateForPIE) override;
+	virtual EDataValidationResult IsDataValid(TArray<FText>& ValidationErrors) override;
 	// --
 #endif
 
