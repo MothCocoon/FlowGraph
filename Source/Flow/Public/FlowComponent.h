06f823999845ebc514e1180d6ea0fd25b147817d
Moth Doctor <krzysiek.justynski@gmail.com>
2021-03-30 23:36:27 +0200

    support for adding/removing Identity Tags during gameplay

--- Source/Flow/Public/FlowComponent.h
+++ Source/Flow/Public/FlowComponent.h
@@ -4,6 +4,8 @@
 #include "GameplayTagContainer.h"
 #include "FlowComponent.generated.h"
 
+class UFlowSubsystem;
+
 DECLARE_MULTICAST_DELEGATE_TwoParams(FFlowComponentNotify, class UFlowComponent*, const FGameplayTag&);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FFlowComponentDynamicNotify, class UFlowComponent*, FlowComponent, const FGameplayTag&, NotifyTag);
 
@@ -15,25 +17,47 @@ class FLOW_API UFlowComponent : public UActorComponent
 {
 	GENERATED_UCLASS_BODY()
 
+//////////////////////////////////////////////////////////////////////////
+// Identity Tags
+	friend class UFlowSubsystem;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Flow")
 	FGameplayTagContainer IdentityTags;
-
-	FFlowComponentNotify OnNotifyFromComponent;
 
 	virtual void BeginPlay() override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Flow")
+	void AddIdentityTag(const FGameplayTag Tag);
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Flow")
+    void AddIdentityTags(FGameplayTagContainer Tags);
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Flow")
+	void RemoveIdentityTag(const FGameplayTag Tag);
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Flow")
+    void RemoveIdentityTags(FGameplayTagContainer Tags);
+
+//////////////////////////////////////////////////////////////////////////
+// Notify Tags: set only in runtime
+	
+	// Send notification from the actor to Flow graphs
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Flow")
+    void NotifyGraph(const FGameplayTag NotifyTag);
+	
+	FFlowComponentNotify OnNotifyFromComponent;
+	
 	// Receive notification from Flow graph or another Flow Component
 	UPROPERTY(BlueprintAssignable, Category = "Flow")
 	FFlowComponentDynamicNotify ReceiveNotify;
-
-	// Send notification from the actor to Flow graphs
-	UFUNCTION(BlueprintCallable, Category = "Flow")
-	void NotifyGraph(const FGameplayTag NotifyTag);
 
 	virtual void NotifyFromGraph(const FGameplayTag NotifyTag);
 
 	// Send notification to another actor containing Flow Component
 	UFUNCTION(BlueprintCallable, Category = "Flow")
 	virtual void NotifyActor(const FGameplayTag ActorTag, const FGameplayTag NotifyTag);
+
+protected:
+	UFlowSubsystem* GetFlowSubsystem() const;
 };
