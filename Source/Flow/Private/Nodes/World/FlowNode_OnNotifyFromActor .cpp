8fd3e0fd8109632a243618e6cc46199b1bedc621
Marat Yakupov <moadib73rus@gmail.com>
2021-04-01 18:15:35 +0400

    PR #28: Allow to specify multiple tags in FlowNode_OnNotifyFromActor

--- Source/Flow/Private/Nodes/World/FlowNode_OnNotifyFromActor .cpp
+++ Source/Flow/Private/Nodes/World/FlowNode_OnNotifyFromActor .cpp
@@ -10,6 +10,16 @@ UFlowNode_OnNotifyFromActor::UFlowNode_OnNotifyFromActor(const FObjectInitialize
 #endif
 }
 
+void UFlowNode_OnNotifyFromActor::PostLoad()
+{
+	Super::PostLoad();
+
+	if (NotifyTag_DEPRECATED.IsValid())
+	{
+		NotifyTags = FGameplayTagContainer(NotifyTag_DEPRECATED);
+	}
+}
+
 void UFlowNode_OnNotifyFromActor::ExecuteInput(const FName& PinName)
 {
 	Super::ExecuteInput(PinName);
@@ -25,7 +35,7 @@ void UFlowNode_OnNotifyFromActor::ObserveActor(TWeakObjectPtr<AActor> Actor, TWe
 		Component->OnNotifyFromComponent.AddWeakLambda(this, [SelfWeakPtr](UFlowComponent* FlowComponent, const FGameplayTag& Tag)
 		{
 			if (SelfWeakPtr.IsValid() && FlowComponent->IdentityTags.HasTagExact(SelfWeakPtr.Get()->IdentityTag)
-				&& (!SelfWeakPtr.Get()->NotifyTag.IsValid() || SelfWeakPtr.Get()->NotifyTag == Tag))
+				&& (!SelfWeakPtr.Get()->NotifyTags.IsValid() || SelfWeakPtr.Get()->NotifyTags.HasTagExact(Tag)))
 			{
 				SelfWeakPtr->TriggerFirstOutput(true);
 			}
@@ -42,7 +52,8 @@ void UFlowNode_OnNotifyFromActor::ForgetActor(TWeakObjectPtr<AActor> Actor, TWea
 FString UFlowNode_OnNotifyFromActor::GetNodeDescription() const
 {
 	const FString IdentityString = IdentityTag.IsValid() ? IdentityTag.ToString() : MissingIdentityTag;
-	const FString NotifyString = NotifyTag.IsValid() ? NotifyTag.ToString() : TEXT("---");
+	const FString NotifyString = !NotifyTags.IsValid() ? TEXT("---") :
+		FString::JoinBy(NotifyTags, LINE_TERMINATOR, [](const FGameplayTag& Tag) { return Tag.ToString(); });
 
 	return IdentityString + LINE_TERMINATOR + NotifyString;
 }
