1707aa7b1e76b05d2de235da3e7ef5ccb313c1b6
Moth Doctor <krzysiek.justynski@gmail.com>
2021-04-08 22:02:12 +0200

    Flow Component now supports multiplayer
    - replicating added/removed Identity Tags
    - replicating recent Notify Tags

--- Source/Flow/Public/FlowComponent.h
+++ Source/Flow/Public/FlowComponent.h
@@ -9,6 +9,28 @@
 class UFlowAsset;
 class UFlowSubsystem;
 
+USTRUCT()
+struct FNotifyTagReplication
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FGameplayTag ActorTag;
+
+	UPROPERTY()
+	FGameplayTag NotifyTag;
+
+	FNotifyTagReplication() {}
+
+	FNotifyTagReplication(const FGameplayTag& InActorTag, const FGameplayTag& InNotifyTag)
+		: ActorTag(InActorTag)
+		, NotifyTag(InNotifyTag)
+	{
+	}
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FFlowComponentTagsReplicated, class UFlowComponent*, FlowComponent, const FGameplayTagContainer&, CurrentTags);
+
 DECLARE_MULTICAST_DELEGATE_TwoParams(FFlowComponentNotify, class UFlowComponent*, const FGameplayTag&);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FFlowComponentDynamicNotify, class UFlowComponent*, FlowComponent, const FGameplayTag&, NotifyTag);
 
@@ -20,53 +42,126 @@ class FLOW_API UFlowComponent : public UActorComponent
 {
 	GENERATED_UCLASS_BODY()
 
+	friend class UFlowSubsystem;
+	
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
 //////////////////////////////////////////////////////////////////////////
 // Identity Tags
-	friend class UFlowSubsystem;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Flow")
 	FGameplayTagContainer IdentityTags;
 
+private:
+	// Used to replicate tags added during gameplay
+	UPROPERTY(ReplicatedUsing = OnRep_AddedIdentityTags)
+	FGameplayTagContainer AddedIdentityTags;
+
+	// Used to replicate tags removed during gameplay
+	UPROPERTY(ReplicatedUsing = OnRep_RemovedIdentityTags)
+	FGameplayTagContainer RemovedIdentityTags;
+
+public:
 	virtual void BeginPlay() override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
-	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Flow")
-	void AddIdentityTag(const FGameplayTag Tag);
+	UFUNCTION(BlueprintCallable, Category = "Flow")
+	void AddIdentityTag(const FGameplayTag Tag, const EFlowNetMode NetMode = EFlowNetMode::Authority);
 
-	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Flow")
-    void AddIdentityTags(FGameplayTagContainer Tags);
+	UFUNCTION(BlueprintCallable, Category = "Flow")
+	void AddIdentityTags(FGameplayTagContainer Tags, const EFlowNetMode NetMode = EFlowNetMode::Authority);
 
-	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Flow")
-	void RemoveIdentityTag(const FGameplayTag Tag);
+	UFUNCTION(BlueprintCallable, Category = "Flow")
+	void RemoveIdentityTag(const FGameplayTag Tag, const EFlowNetMode NetMode = EFlowNetMode::Authority);
 
-	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Flow")
-    void RemoveIdentityTags(FGameplayTagContainer Tags);
+	UFUNCTION(BlueprintCallable, Category = "Flow")
+	void RemoveIdentityTags(FGameplayTagContainer Tags, const EFlowNetMode NetMode = EFlowNetMode::Authority);
+
+private:
+	UFUNCTION()
+	void OnRep_AddedIdentityTags();
+
+	UFUNCTION()
+	void OnRep_RemovedIdentityTags();
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Flow")
+	FFlowComponentTagsReplicated OnIdentityTagsAdded;
+
+	UPROPERTY(BlueprintAssignable, Category = "Flow")
+	FFlowComponentTagsReplicated OnIdentityTagsRemoved;
 
 //////////////////////////////////////////////////////////////////////////
-// Notify Tags: set only in runtime
-	
-	// Send notification from the actor to Flow graphs
-	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Flow")
-    void NotifyGraph(const FGameplayTag NotifyTag);
+// Component sending Notify Tags to Flow Graph, or any other listener
 
+private:
+	// Stores only recently sent tags
+	UPROPERTY(ReplicatedUsing = OnRep_SentNotifyTags)
+	FGameplayTagContainer RecentlySentNotifyTags;
+
+public:
+	FGameplayTagContainer GetRecentlySentNotifyTags() const { return RecentlySentNotifyTags; }
+
+	// Send single notification from the actor to Flow graphs
+	// If set on server, it always going to be replicated to clients
+	UFUNCTION(BlueprintCallable, Category = "Flow")
+	void NotifyGraph(const FGameplayTag NotifyTag, const EFlowNetMode NetMode = EFlowNetMode::Authority);
+
+	// Send multiple notifications at once - from the actor to Flow graphs
+	// If set on server, it always going to be replicated to clients
+	UFUNCTION(BlueprintCallable, Category = "Flow")
+	void BulkNotifyGraph(const FGameplayTagContainer NotifyTags, const EFlowNetMode NetMode = EFlowNetMode::Authority);
+
+private:
+	UFUNCTION()
+	void OnRep_SentNotifyTags();
+
+public:
 	FFlowComponentNotify OnNotifyFromComponent;
-	
+
+//////////////////////////////////////////////////////////////////////////
+// Component receiving Notify Tags from Flow Graph
+
+private:
+	// Stores only recently replicated tags
+	UPROPERTY(ReplicatedUsing = OnRep_NotifyTagsFromGraph)
+	FGameplayTagContainer NotifyTagsFromGraph;
+
+public:
+	virtual void NotifyFromGraph(const FGameplayTagContainer& NotifyTags, const EFlowNetMode NetMode = EFlowNetMode::Authority);
+
+private:
+	UFUNCTION()
+	void OnRep_NotifyTagsFromGraph();
+
+public:
 	// Receive notification from Flow graph or another Flow Component
 	UPROPERTY(BlueprintAssignable, Category = "Flow")
 	FFlowComponentDynamicNotify ReceiveNotify;
 
-	virtual void NotifyFromGraph(const FGameplayTagContainer& NotifyTags);
+//////////////////////////////////////////////////////////////////////////
+// Sending Notify Tags between Flow components
 
+private:
+	// Stores only recently replicated tags
+	UPROPERTY(ReplicatedUsing = OnRep_NotifyTagsFromAnotherComponent)
+	TArray<FNotifyTagReplication> NotifyTagsFromAnotherComponent;
+
+public:
 	// Send notification to another actor containing Flow Component
 	UFUNCTION(BlueprintCallable, Category = "Flow")
-	virtual void NotifyActor(const FGameplayTag ActorTag, const FGameplayTag NotifyTag);
+	virtual void NotifyActor(const FGameplayTag ActorTag, const FGameplayTag NotifyTag, const EFlowNetMode NetMode = EFlowNetMode::Authority);
+
+private:
+	UFUNCTION()
+	void OnRep_NotifyTagsFromAnotherComponent();
 
 //////////////////////////////////////////////////////////////////////////
 // Root Flow
 
 public:
 	// Asset that might instantiated as "Root Flow" 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "RootFlow")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RootFlow")
 	UFlowAsset* RootFlow;
 
 	// If true, component will start Root Flow on Begin Play
@@ -95,7 +190,7 @@ public:
 
 //////////////////////////////////////////////////////////////////////////
 // Helpers
-	
+
 	UFlowSubsystem* GetFlowSubsystem() const;
 	bool IsFlowNetMode(const EFlowNetMode NetMode) const;
 };
