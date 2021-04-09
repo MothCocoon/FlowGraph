f177ff6a0117f61e4a171990fee637aa06de530f
Moth Doctor <krzysiek.justynski@gmail.com>
2021-04-09 12:55:05 +0200

    cosmetic polishing

--- Source/Flow/Private/Nodes/Route/FlowNode_ExecutionMultiGate.cpp
+++ Source/Flow/Private/Nodes/Route/FlowNode_ExecutionMultiGate.cpp
@@ -1,88 +1,83 @@
 #include "Nodes/Route/FlowNode_ExecutionMultiGate.h"
 
-#include "Misc/StringBuilder.h"
-
-const FName UFlowNode_ExecutionMultiGate::ResetInputName = TEXT("Reset");
-
 UFlowNode_ExecutionMultiGate::UFlowNode_ExecutionMultiGate(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
+	, StartIndex(INDEX_NONE)
 {
 #if WITH_EDITOR
 	Category = TEXT("Route");
 	NodeStyle = EFlowNodeStyle::Logic;
 #endif
 
-	InputNames.Empty();
-	InputNames.Add(DefaultInputName);
-	InputNames.Add(ResetInputName);
-	
+	InputNames.Add(TEXT("Reset"));
 	SetNumericalOutputs(0, 1);
 }
 
 void UFlowNode_ExecutionMultiGate::ExecuteInput(const FName& PinName)
 {
-	if (PinName == ResetInputName)
+	if (PinName == DefaultInputName)
 	{
-		Finish();
-		return;
-	}
-
-	if (Completed.Num() == 0)
-	{
-		Completed.Init(false, OutputNames.Num());
-	}
-
-	if (!Completed.Contains(false))
-	{
-		return;
-	}
-
-	const bool bUseStartIndex = !Completed.Contains(true) && Completed.IsValidIndex(StartIndex);
-	
-	if (bRandom)
-	{
-		int32 Index = INDEX_NONE;
-		if (bUseStartIndex)
+		if (Completed.Num() == 0)
 		{
-			Index = StartIndex;
+			Completed.Init(false, OutputNames.Num());
+		}
+
+		if (!Completed.Contains(false))
+		{
+			return;
+		}
+
+		const bool bUseStartIndex = !Completed.Contains(true) && Completed.IsValidIndex(StartIndex);
+
+		if (bRandom)
+		{
+			int32 Index;
+			if (bUseStartIndex)
+			{
+				Index = StartIndex;
+			}
+			else
+			{
+				TArray<int32> AvailableIndexes;
+				AvailableIndexes.Reserve(Completed.Num()); // todo
+
+				for (int32 i = 0; i < Completed.Num(); i++)
+				{
+					if (Completed[i] == false)
+					{
+						AvailableIndexes.Emplace(i);
+					}
+				}
+
+				const int32 Random = FMath::RandRange(0, AvailableIndexes.Num() - 1);
+				Index = AvailableIndexes[Random];
+			}
+
+			Completed[Index] = true;
+			TriggerOutput(OutputNames[Index], false);
 		}
 		else
 		{
-			TArray<int32> AvailableIndexes;
-			AvailableIndexes.Reserve(Completed.Num());
-
-			for (int32 Idx = 0; Idx < Completed.Num(); Idx++)
+			if (bUseStartIndex)
 			{
-				if (Completed[Idx] == false)
-				{
-					AvailableIndexes.Add(Idx);
-				}
+				NextOutput = StartIndex;
 			}
 
-			const int32 Random = FMath::RandRange(0, AvailableIndexes.Num() - 1);
-			Index = AvailableIndexes[Random];
+			const int32 CurrentOutput = NextOutput;
+			// We have to calculate NextOutput before TriggerOutput(..)
+			// TriggerOutput may call Reset and Cleanup
+			NextOutput = ++NextOutput % OutputNames.Num();
+
+			Completed[CurrentOutput] = true;
+			TriggerOutput(OutputNames[CurrentOutput], false);
 		}
 
-		Completed[Index] = true;
-		TriggerOutput(OutputNames[Index], false);
-	}
-	else
-	{
-		if (bUseStartIndex)
+		if (!Completed.Contains(false) && bLoop)
 		{
-			NextOutput = StartIndex;
+			Finish();
 		}
-
-		const int32 CurrentOutput = NextOutput;
-		//We have to calculate NextOutput before TriggerOutput(..)
-		//TriggerOutput may call Reset and Cleanup
-		NextOutput = ++NextOutput % OutputNames.Num();
-
-		Completed[CurrentOutput] = true;
-		TriggerOutput(OutputNames[CurrentOutput], false);
 	}
-
-	if (!Completed.Contains(false) && bLoop)
+	else if (PinName == TEXT("Reset"))
 	{
 		Finish();
 	}
@@ -97,28 +92,41 @@ void UFlowNode_ExecutionMultiGate::Cleanup()
 #if WITH_EDITOR
 FString UFlowNode_ExecutionMultiGate::GetNodeDescription() const
 {
-	TStringBuilder<128> Builder;
+	FString Result;
+	Result.Reserve(128);
 
 	if (bRandom)
-		Builder += TEXT("Random");
+	{
+		Result.Append(TEXT("Random"));
+	}
 
 	if (bRandom && bLoop)
-		Builder += TEXT(", ");
-	
+	{
+		Result.Append(TEXT(", "));
+	}
+
 	if (bLoop)
-		Builder += TEXT("Loop");
+	{
+		Result.Append(TEXT("Loop"));
+	}
 
 	if (StartIndex != INDEX_NONE)
 	{
 		if (bRandom || bLoop)
-			Builder += TEXT(", ");
-		
+		{
+			Result.Append(TEXT(", "));
+		}
+
 		if (OutputNames.IsValidIndex(StartIndex))
-			Builder += TEXT("StartIndex: ") + FString::FromInt(StartIndex);
+		{
+			Result.Appendf(TEXT("Start Index: %d"), StartIndex);
+		}
 		else
-			Builder += TEXT("StartIndex: Invalid");
+		{
+			Result.Append(TEXT("StartIndex: Invalid"));
+		}
 	}
-	
-	return Builder.ToString();
+
+	return Result;
 }
 #endif
--- Source/Flow/Public/Nodes/Route/FlowNode_ExecutionMultiGate.h
+++ Source/Flow/Public/Nodes/Route/FlowNode_ExecutionMultiGate.h
@@ -6,24 +6,34 @@
 /**
  * Executes a series of pins in order
  */
-UCLASS(NotBlueprintable, meta = (DisplayName = "MultiGate"))
+UCLASS(NotBlueprintable, meta = (DisplayName = "Multi Gate"))
 class FLOW_API UFlowNode_ExecutionMultiGate final : public UFlowNode
 {
 	GENERATED_UCLASS_BODY()
-public:
+
 	UPROPERTY(EditDefaultsOnly, Category = "MultiGate")
 	bool bRandom;
 
+	// Allow executing output pins again, without triggering Reset pin
+	// If set to False, every output pin can be triggered only once
 	UPROPERTY(EditDefaultsOnly, Category = "MultiGate")
 	bool bLoop;
 
 	UPROPERTY(EditDefaultsOnly, Category = "MultiGate")
-	int32 StartIndex = INDEX_NONE;
-	
+	int32 StartIndex;
+
+private:
+	UPROPERTY()
+	int32 NextOutput;
+
+	UPROPERTY()
+	TArray<bool> Completed;
+
+public:
 #if WITH_EDITOR
 	virtual bool CanUserAddOutput() const override { return true; }
 #endif
-	
+
 protected:
 	virtual void ExecuteInput(const FName& PinName) override;
 	virtual void Cleanup() override;
@@ -31,12 +41,4 @@ protected:
 #if WITH_EDITOR
 	virtual FString GetNodeDescription() const override;
 #endif
-
-	UPROPERTY()
-	int32 NextOutput;
-
-	UPROPERTY()
-	TArray<bool> Completed;
-
-	static const FName ResetInputName;
 };
