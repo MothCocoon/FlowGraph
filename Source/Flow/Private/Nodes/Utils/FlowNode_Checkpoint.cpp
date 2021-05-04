ba942cf681d4e56bdd00346bb9eb520180ad9bf3
Moth Doctor <krzysiek.justynski@gmail.com>
2021-05-04 13:58:38 +0200

    SaveGame, first iteration WIP
    - saving Flow Asset Instances (Root Flow and Sub Graphs), Flow Nodes
    - saving components in progress
    - Save system self-contained in plugin, it might be overridden by game's save systems

---
+++ Source/Flow/Private/Nodes/Utils/FlowNode_Checkpoint.cpp
@@ -0,0 +1,18 @@
+﻿#include "Nodes/Utils/FlowNode_Checkpoint.h"
+#include "FlowSubsystem.h"
+
+UFlowNode_Checkpoint::UFlowNode_Checkpoint(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+#if WITH_EDITOR
+	Category = TEXT("Utils");
+#endif
+}
+
+void UFlowNode_Checkpoint::ExecuteInput(const FName& PinName)
+{
+	if (GetFlowSubsystem())
+	{
+		GetFlowSubsystem()->SaveGame();
+	}
+}
