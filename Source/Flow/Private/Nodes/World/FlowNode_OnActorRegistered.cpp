f1aeef81e67015c93eef38a85a6a06ba828bcb30
Moth Doctor <krzysiek.justynski@gmail.com>
2021-08-04 21:04:09 +0200

    new nodes: On Actor Registered, On Actor Unregistered

---
+++ Source/Flow/Private/Nodes/World/FlowNode_OnActorRegistered.cpp
@@ -0,0 +1,20 @@
+#include "Nodes/World/FlowNode_OnActorRegistered.h"
+
+UFlowNode_OnActorRegistered::UFlowNode_OnActorRegistered(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UFlowNode_OnActorRegistered::ExecuteInput(const FName& PinName)
+{
+	Super::ExecuteInput(PinName);
+}
+
+void UFlowNode_OnActorRegistered::ObserveActor(TWeakObjectPtr<AActor> Actor, TWeakObjectPtr<UFlowComponent> Component)
+{
+	if (!RegisteredActors.Contains(Actor))
+	{
+		RegisteredActors.Emplace(Actor, Component);
+		OnEventReceived();
+	}
+}
