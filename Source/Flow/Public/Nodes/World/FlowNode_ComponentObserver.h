b7e4a96a5b6b0535163252177af2359fe1292197
Moth Doctor <krzysiek.justynski@gmail.com>
2021-09-03 00:51:36 +0200

    added Identity Match Type, improves Component Observer

--- Source/Flow/Public/Nodes/World/FlowNode_ComponentObserver.h
+++ Source/Flow/Public/Nodes/World/FlowNode_ComponentObserver.h
@@ -15,12 +15,17 @@ UCLASS(Abstract, NotBlueprintable)
 class FLOW_API UFlowNode_ComponentObserver : public UFlowNode
 {
 	GENERATED_UCLASS_BODY()
-
-    friend class FFlowNode_ComponentObserverDetails;
 	
+	friend class FFlowNode_ComponentObserverDetails;
+
 protected:
 	UPROPERTY(EditAnywhere, Category = "ObservedComponent")
 	FGameplayTagContainer IdentityTags;
+
+	// Container A: Identity Tags in Flow Component
+	// Container B: Identity Tags listed above
+	UPROPERTY(EditAnywhere, Category = "ObservedComponent")
+	EFlowTagContainerMatchType IdentityMatchType;
 
 	// This node will become Completed, if Success Limit > 0 and Success Count reaches this limit
 	// Set this to zero, if you'd like receive events indefinitely (node would finish work only if explicitly Stopped)
@@ -30,12 +35,12 @@ protected:
 	// This node will become Completed, if Success Limit > 0 and Success Count reaches this limit
 	UPROPERTY(VisibleAnywhere, Category = "Lifetime")
 	int32 SuccessCount;
-	
+
 	TMap<TWeakObjectPtr<AActor>, TWeakObjectPtr<UFlowComponent>> RegisteredActors;
 
 protected:
 	virtual void PostLoad() override;
-	
+
 	virtual void ExecuteInput(const FName& PinName) override;
 	virtual void OnLoad_Implementation() override;
 
@@ -46,20 +51,20 @@ protected:
 	virtual void OnComponentRegistered(UFlowComponent* Component);
 
 	UFUNCTION()
-    virtual void OnComponentTagAdded(UFlowComponent* Component, const FGameplayTagContainer& AddedTags);
+	virtual void OnComponentTagAdded(UFlowComponent* Component, const FGameplayTagContainer& AddedTags);
+
+	UFUNCTION()
+	virtual void OnComponentTagRemoved(UFlowComponent* Component, const FGameplayTagContainer& RemovedTags);
 
 	UFUNCTION()
 	virtual void OnComponentUnregistered(UFlowComponent* Component);
-
-	UFUNCTION()
-    virtual void OnComponentTagRemoved(UFlowComponent* Component, const FGameplayTagContainer& RemovedTags);
 
 	virtual void ObserveActor(TWeakObjectPtr<AActor> Actor, TWeakObjectPtr<UFlowComponent> Component) {}
 	virtual void ForgetActor(TWeakObjectPtr<AActor> Actor, TWeakObjectPtr<UFlowComponent> Component) {}
 
 	UFUNCTION()
 	virtual void OnEventReceived();
-	
+
 	virtual void Cleanup() override;
 
 #if WITH_EDITOR
