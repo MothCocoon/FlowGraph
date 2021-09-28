6abc32adb151a2a869bc8280e3597345a76eafa1
Moth Doctor <krzysiek.justynski@gmail.com>
2021-09-28 20:59:50 +0200

    removed redundant Extensibility Managers

--- Source/FlowEditor/Public/FlowEditorModule.h
+++ Source/FlowEditor/Public/FlowEditorModule.h
@@ -4,31 +4,12 @@
 #include "IAssetTypeActions.h"
 #include "Modules/ModuleInterface.h"
 #include "PropertyEditorDelegates.h"
-#include "Toolkits/AssetEditorToolkit.h"
 
 class FSlateStyleSet;
 struct FGraphPanelPinConnectionFactory;
 
 class FFlowAssetEditor;
 class UFlowAsset;
-
-struct FExtensibilityManagers
-{
-	TSharedPtr<FExtensibilityManager> MenuExtensibilityManager;
-	TSharedPtr<FExtensibilityManager> ToolBarExtensibilityManager;
-
-	void Init()
-	{
-		MenuExtensibilityManager = MakeShareable(new FExtensibilityManager);
-		ToolBarExtensibilityManager = MakeShareable(new FExtensibilityManager);
-	}
-
-	void Reset()
-	{
-		MenuExtensibilityManager.Reset();
-		ToolBarExtensibilityManager.Reset();
-	}
-};
 
 DECLARE_LOG_CATEGORY_EXTERN(LogFlowEditor, Log, All)
 
@@ -41,7 +22,6 @@ private:
 	TArray<TSharedRef<IAssetTypeActions>> RegisteredAssetActions;
 	TSet<FName> CustomClassLayouts;
 	TSharedPtr<FGraphPanelPinConnectionFactory> FlowGraphConnectionFactory;
-	FExtensibilityManagers FlowAssetExtensibility;
 
 public:
 	virtual void StartupModule() override;
@@ -55,14 +35,11 @@ private:
 	void RegisterCustomClassLayout(const TSubclassOf<UObject> Class, const FOnGetDetailCustomizationInstance DetailLayout);
 
 public:
-	TSharedPtr<FExtensibilityManager> GetFlowAssetMenuExtensibilityManager() const;
-	TSharedPtr<FExtensibilityManager> GetFlowAssetToolBarExtensibilityManager() const;
-
 	FDelegateHandle FlowTrackCreateEditorHandle;
 
 private:
 	void CreateFlowToolbar(FToolBarBuilder& ToolbarBuilder) const;
 
 public:
-	TSharedRef<FFlowAssetEditor> CreateFlowAssetEditor(const EToolkitMode::Type Mode, const TSharedPtr<IToolkitHost>& InitToolkitHost, UFlowAsset* FlowAsset);
+	static TSharedRef<FFlowAssetEditor> CreateFlowAssetEditor(const EToolkitMode::Type Mode, const TSharedPtr<IToolkitHost>& InitToolkitHost, UFlowAsset* FlowAsset);
 };
