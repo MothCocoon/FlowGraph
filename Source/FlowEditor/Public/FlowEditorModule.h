3d86913eb51ed8a399dda89667cb44ba7151366d
Moth Doctor <krzysiek.justynski@gmail.com>
2021-09-28 21:49:23 +0200

    Revert "removed redundant Extensibility Managers"
    This reverts commit 6abc32adb151a2a869bc8280e3597345a76eafa1.

--- Source/FlowEditor/Public/FlowEditorModule.h
+++ Source/FlowEditor/Public/FlowEditorModule.h
@@ -4,12 +4,31 @@
 #include "IAssetTypeActions.h"
 #include "Modules/ModuleInterface.h"
 #include "PropertyEditorDelegates.h"
+#include "Toolkits/AssetEditorToolkit.h"
 
 class FSlateStyleSet;
 struct FGraphPanelPinConnectionFactory;
 
 class FFlowAssetEditor;
 class UFlowAsset;
+
+struct FExtensibilityManagers
+{
+	TSharedPtr<FExtensibilityManager> MenuExtensibilityManager;
+	TSharedPtr<FExtensibilityManager> ToolBarExtensibilityManager;
+
+	void Init()
+	{
+		MenuExtensibilityManager = MakeShareable(new FExtensibilityManager);
+		ToolBarExtensibilityManager = MakeShareable(new FExtensibilityManager);
+	}
+
+	void Reset()
+	{
+		MenuExtensibilityManager.Reset();
+		ToolBarExtensibilityManager.Reset();
+	}
+};
 
 DECLARE_LOG_CATEGORY_EXTERN(LogFlowEditor, Log, All)
 
@@ -22,6 +41,7 @@ private:
 	TArray<TSharedRef<IAssetTypeActions>> RegisteredAssetActions;
 	TSet<FName> CustomClassLayouts;
 	TSharedPtr<FGraphPanelPinConnectionFactory> FlowGraphConnectionFactory;
+	FExtensibilityManagers FlowAssetExtensibility;
 
 public:
 	virtual void StartupModule() override;
@@ -35,11 +55,14 @@ private:
 	void RegisterCustomClassLayout(const TSubclassOf<UObject> Class, const FOnGetDetailCustomizationInstance DetailLayout);
 
 public:
+	TSharedPtr<FExtensibilityManager> GetFlowAssetMenuExtensibilityManager() const;
+	TSharedPtr<FExtensibilityManager> GetFlowAssetToolBarExtensibilityManager() const;
+
 	FDelegateHandle FlowTrackCreateEditorHandle;
 
 private:
 	void CreateFlowToolbar(FToolBarBuilder& ToolbarBuilder) const;
 
 public:
-	static TSharedRef<FFlowAssetEditor> CreateFlowAssetEditor(const EToolkitMode::Type Mode, const TSharedPtr<IToolkitHost>& InitToolkitHost, UFlowAsset* FlowAsset);
+	TSharedRef<FFlowAssetEditor> CreateFlowAssetEditor(const EToolkitMode::Type Mode, const TSharedPtr<IToolkitHost>& InitToolkitHost, UFlowAsset* FlowAsset);
 };
