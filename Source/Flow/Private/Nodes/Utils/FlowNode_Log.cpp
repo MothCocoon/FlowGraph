aa68c4b4162fd6303998a081ebb310062066fa5f
Moth Doctor <krzysiek.justynski@gmail.com>
2021-10-25 22:53:57 +0200

    removed Fatal option, copy-pasted tooltips from ELogVerbosity

--- Source/Flow/Private/Nodes/Utils/FlowNode_Log.cpp
+++ Source/Flow/Private/Nodes/Utils/FlowNode_Log.cpp
@@ -6,6 +6,7 @@
 UFlowNode_Log::UFlowNode_Log(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 	, Message(TEXT("Log!"))
+	, Verbosity(EFlowLogVerbosity::Warning)
 	, bPrintToScreen(true)
 	, Duration(5.0f)
 	, TextColor(FColor::Yellow)
@@ -19,14 +20,17 @@ void UFlowNode_Log::ExecuteInput(const FName& PinName)
 {
 	switch (Verbosity)
 	{
-		case EFlowLogVerbosity::Log:
-			UE_LOG(LogFlow, Log, TEXT("%s"), *Message);
+		case EFlowLogVerbosity::Error:
+			UE_LOG(LogFlow, Error, TEXT("%s"), *Message);
 			break;
 		case EFlowLogVerbosity::Warning:
 			UE_LOG(LogFlow, Warning, TEXT("%s"), *Message);
 			break;
-		case EFlowLogVerbosity::Error:
-			UE_LOG(LogFlow, Error, TEXT("%s"), *Message);
+		case EFlowLogVerbosity::Display:
+			UE_LOG(LogFlow, Display, TEXT("%s"), *Message);
+			break;
+		case EFlowLogVerbosity::Log:
+			UE_LOG(LogFlow, Log, TEXT("%s"), *Message);
 			break;
 		case EFlowLogVerbosity::Verbose:
 			UE_LOG(LogFlow, Verbose, TEXT("%s"), *Message);
@@ -34,9 +38,7 @@ void UFlowNode_Log::ExecuteInput(const FName& PinName)
 		case EFlowLogVerbosity::VeryVerbose:
 			UE_LOG(LogFlow, VeryVerbose, TEXT("%s"), *Message);
 			break;
-		case EFlowLogVerbosity::Fatal:
-			UE_LOG(LogFlow, Fatal, TEXT("%s"), *Message);
-		default:;
+		default: ;
 	}
 
 	if (bPrintToScreen)
@@ -50,11 +52,6 @@ void UFlowNode_Log::ExecuteInput(const FName& PinName)
 #if WITH_EDITOR
 FString UFlowNode_Log::GetNodeDescription() const
 {
-	if (Verbosity == EFlowLogVerbosity::Fatal)
-	{
-		return FString::Printf(TEXT("FATAL LOG: %s"), *Message);
-	}
-
 	return Message;
 }
 #endif
