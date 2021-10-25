57240f4b8ab2b708e49ad69d7c6fcf8121477768
Satheesh (ryanjon2040) <ryanjon2040@users.noreply.github.com>
2021-10-26 01:17:52 +0530

    Add log verbosity (#58)

--- Source/Flow/Private/Nodes/Utils/FlowNode_Log.cpp
+++ Source/Flow/Private/Nodes/Utils/FlowNode_Log.cpp
@@ -17,7 +17,27 @@ UFlowNode_Log::UFlowNode_Log(const FObjectInitializer& ObjectInitializer)
 
 void UFlowNode_Log::ExecuteInput(const FName& PinName)
 {
-	UE_LOG(LogFlow, Warning, TEXT("%s"), *Message);
+	switch (Verbosity)
+	{
+		case EFlowLogVerbosity::Log:
+			UE_LOG(LogFlow, Log, TEXT("%s"), *Message);
+			break;
+		case EFlowLogVerbosity::Warning:
+			UE_LOG(LogFlow, Warning, TEXT("%s"), *Message);
+			break;
+		case EFlowLogVerbosity::Error:
+			UE_LOG(LogFlow, Error, TEXT("%s"), *Message);
+			break;
+		case EFlowLogVerbosity::Verbose:
+			UE_LOG(LogFlow, Verbose, TEXT("%s"), *Message);
+			break;
+		case EFlowLogVerbosity::VeryVerbose:
+			UE_LOG(LogFlow, VeryVerbose, TEXT("%s"), *Message);
+			break;
+		case EFlowLogVerbosity::Fatal:
+			UE_LOG(LogFlow, Fatal, TEXT("%s"), *Message);
+		default:;
+	}
 
 	if (bPrintToScreen)
 	{
@@ -30,6 +50,11 @@ void UFlowNode_Log::ExecuteInput(const FName& PinName)
 #if WITH_EDITOR
 FString UFlowNode_Log::GetNodeDescription() const
 {
+	if (Verbosity == EFlowLogVerbosity::Fatal)
+	{
+		return FString::Printf(TEXT("FATAL LOG: %s"), *Message);
+	}
+
 	return Message;
 }
 #endif
