d7a51d09317d370cdf6254f01589886d41e0b32b
Moth Doctor <krzysiek.justynski@gmail.com>
2021-10-26 12:47:21 +0200

    removed duplicated GetNodeInstance method
    + const correctness

--- Source/Flow/Private/Nodes/FlowNode.cpp
+++ Source/Flow/Private/Nodes/FlowNode.cpp
@@ -446,7 +446,7 @@ UFlowNode* UFlowNode::GetInspectedInstance() const
 {
 	if (const UFlowAsset* FlowInstance = GetFlowAsset()->GetInspectedInstance())
 	{
-		return FlowInstance->GetNodeInstance(GetGuid());
+		return FlowInstance->GetNode(GetGuid());
 	}
 
 	return nullptr;
