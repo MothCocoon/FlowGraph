86ab3af32c3b197d0bb68f5fa45f922f6817d3a2
ryanjon2040 <mindfreak2040@gmail.com>
2021-10-30 17:38:37 +0530

    Add delay node

---
+++ Source/Flow/Private/Nodes/Route/FlowNode_Delay.cpp
@@ -0,0 +1,60 @@
+﻿#include "Nodes/Route/FlowNode_Delay.h"
+
+#define FLOW_CPP_DELAY(TimeInSeconds, ...) GetWorld()->GetTimerManager().SetTimer(TimerHandle_Delay, FTimerDelegate().CreateLambda(__VA_ARGS__), TimeInSeconds, false);
+
+UFlowNode_Delay::UFlowNode_Delay()
+{
+#if WITH_EDITOR
+	Category = TEXT("Route");
+	NodeStyle = EFlowNodeStyle::Condition;
+#endif
+
+	DelayInSeconds = 0.2;
+
+	OutputPins.Empty();
+	OutputPins.Add(FFlowPin(TEXT("Finished"), TEXT("Executes after 'Delay In Seconds' time")));
+}
+
+void UFlowNode_Delay::ExecuteInput(const FName& PinName)
+{
+	if (DelayInSeconds < KINDA_SMALL_NUMBER)
+	{
+		TriggerFirstOutput(true);
+	}
+	else
+	{
+		FLOW_CPP_DELAY(DelayInSeconds, [&]()
+		{
+			TriggerFirstOutput(true);
+		});
+	}
+}
+
+void UFlowNode_Delay::Cleanup()
+{
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle_Delay);
+}
+
+#if WITH_EDITOR
+FString UFlowNode_Delay::GetNodeDescription() const
+{
+	if (DelayInSeconds < KINDA_SMALL_NUMBER)
+	{
+		return "Executes instantly";
+	}
+	
+	return FString::Printf(TEXT("Delay: %.4f"), DelayInSeconds);
+}
+
+FString UFlowNode_Delay::GetStatusString() const
+{
+	if (GetWorld() && DelayInSeconds > KINDA_SMALL_NUMBER)
+	{
+		return FString::Printf(TEXT("Remaining: %.2f"), GetWorld()->GetTimerManager().GetTimerRemaining(TimerHandle_Delay));
+	}
+
+	return "";
+}
+#endif
+
+#undef FLOW_CPP_DELAY
---
+++ Source/Flow/Public/Nodes/Route/FlowNode_Delay.h
@@ -0,0 +1,37 @@
+﻿// Created by Satheesh (ryanjon2040)
+
+#pragma once
+
+#include "Nodes/FlowNode.h"
+#include "FlowNode_Delay.generated.h"
+
+/**
+* Delays execution for given seconds
+*/
+UCLASS(NotBlueprintable, meta = (DisplayName = "Delay"))
+class FLOW_API UFlowNode_Delay final : public UFlowNode
+{
+	GENERATED_BODY()
+
+private:
+
+	FTimerHandle TimerHandle_Delay;
+
+protected:
+	/** Executes 'Finished' after given seconds. */
+	UPROPERTY(EditAnywhere, Category = "Delay", meta = (UIMin = 0, ClampMin = 0, UIMax = 60))
+	float DelayInSeconds;
+
+public:
+
+	UFlowNode_Delay();
+
+protected:
+	virtual void ExecuteInput(const FName& PinName) override;
+	virtual void Cleanup() override;
+
+#if WITH_EDITOR
+	virtual FString GetNodeDescription() const override;
+	virtual FString GetStatusString() const override;
+#endif
+};
