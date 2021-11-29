cfdeab9af0e0487a856f02f0b12ef70d333c2a3d
Sergey Vikhirev <truesmb@gmail.com>
2021-11-29 23:27:01 +0400

    Clear previously selected nodes before new node creation (#77)

--- Source/FlowEditor/Private/Asset/FlowAssetEditor.cpp
+++ Source/FlowEditor/Private/Asset/FlowAssetEditor.cpp
@@ -633,6 +633,7 @@ void FFlowAssetEditor::OnSelectedNodesChanged(const TSet<UObject*>& Nodes)
 
 void FFlowAssetEditor::SelectSingleNode(UEdGraphNode* Node) const
 {
+	FocusedGraphEditor->ClearSelectionSet();
 	FocusedGraphEditor->SetNodeSelection(Node, true);
 }
 
