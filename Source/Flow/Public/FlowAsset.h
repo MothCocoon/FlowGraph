ac04c1fe612b456f5d4ec9bacabf447a5c7e2524
Moth Doctor <krzysiek.justynski@gmail.com>
2021-11-29 21:09:57 +0100

    Mark entire HarvestNodeConnections method as editor-only

--- Source/Flow/Public/FlowAsset.h
+++ Source/Flow/Public/FlowAsset.h
@@ -111,10 +111,10 @@ public:
 
 	void RegisterNode(const FGuid& NewGuid, UFlowNode* NewNode);
 	void UnregisterNode(const FGuid& NodeGuid);
-#endif
 
 	// Processes all nodes and creates map of all pin connections
 	void HarvestNodeConnections();
+#endif
 
 	UFlowNode* GetNode(const FGuid& Guid) const;
 	TMap<FGuid, UFlowNode*> GetNodes() const { return Nodes; }
