b9985506c25556a23973f14451a10536f146a0e1
Sergey Vikhirev <truesmb@gmail.com>
2021-11-29 23:26:42 +0400

    Make UFlowSubsystem::CreateRootFlow public (#76)

--- Source/Flow/Public/FlowSubsystem.h
+++ Source/Flow/Public/FlowSubsystem.h
@@ -61,12 +61,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "FlowSubsystem")
 	void StartRootFlow(UObject* Owner, UFlowAsset* FlowAsset, const bool bAllowMultipleInstances = true);
 
-protected:
 	UFlowAsset* CreateRootFlow(UObject* Owner, UFlowAsset* FlowAsset, const bool bAllowMultipleInstances = true);
 
-	// Finish Policy value is read by Flow Node
-	// Nodes have opportunity to terminate themselves differently if Flow Graph has been aborted
-	// Example: Spawn node might despawn all actors if Flow Graph is aborted, not completed
+	// Finish the root Flow, typically when closing world that created this flow
 	UFUNCTION(BlueprintCallable, Category = "FlowSubsystem")
 	void FinishRootFlow(UObject* Owner, const EFlowFinishPolicy FinishPolicy);
 
