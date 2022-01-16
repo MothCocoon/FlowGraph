98a5d7689e117fc83524a260554b281db35c6f17
Moth Doctor <krzysiek.justynski@gmail.com>
2022-01-16 20:31:12 +0100

    allow to customize Flow Asset Category Name

--- Source/FlowEditor/Private/FlowEditorModule.cpp
+++ Source/FlowEditor/Private/FlowEditorModule.cpp
@@ -29,7 +29,7 @@
 #include "LevelEditor.h"
 #include "Modules/ModuleManager.h"
 
-#define LOCTEXT_NAMESPACE "FlowEditor"
+#define LOCTEXT_NAMESPACE "FlowEditorModule"
 
 EAssetTypeCategories::Type FFlowEditorModule::FlowAssetCategory = static_cast<EAssetTypeCategories::Type>(0);
 
@@ -111,7 +111,7 @@ void FFlowEditorModule::ShutdownModule()
 void FFlowEditorModule::RegisterAssets()
 {
 	IAssetTools& AssetTools = FModuleManager::LoadModuleChecked<FAssetToolsModule>("AssetTools").Get();
-	FlowAssetCategory = AssetTools.RegisterAdvancedAssetCategory(FName(TEXT("Flow")), LOCTEXT("FlowAssetCategory", "Flow"));
+	FlowAssetCategory = AssetTools.RegisterAdvancedAssetCategory(FName(TEXT("Flow")), UFlowGraphSettings::Get()->FlowAssetCategoryName);
 
 	const TSharedRef<IAssetTypeActions> FlowAssetActions = MakeShareable(new FAssetTypeActions_FlowAsset());
 	RegisteredAssetActions.Add(FlowAssetActions);
