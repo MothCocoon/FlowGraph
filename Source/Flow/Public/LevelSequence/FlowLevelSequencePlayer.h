fa7aba1ead4096904a6bd2836fdb7c4a8a6071df
seimei0083 <dreadshell@gmail.com>
2022-05-10 12:02:48 +0900

    Implemented the feature that can set the flow graph asset owner actor as the original start point for the level sequence.

--- Source/Flow/Public/LevelSequence/FlowLevelSequencePlayer.h
+++ Source/Flow/Public/LevelSequence/FlowLevelSequencePlayer.h
@@ -20,7 +20,7 @@ private:
 
 public:
     // variant of ULevelSequencePlayer::CreateLevelSequencePlayer
-	static UFlowLevelSequencePlayer* CreateFlowLevelSequencePlayer(UObject* WorldContextObject, ULevelSequence* LevelSequence, FMovieSceneSequencePlaybackSettings Settings, FLevelSequenceCameraSettings CameraSettings, ALevelSequenceActor*& OutActor);
+	static UFlowLevelSequencePlayer* CreateFlowLevelSequencePlayer(UObject* WorldContextObject, ULevelSequence* LevelSequence, FMovieSceneSequencePlaybackSettings Settings, FLevelSequenceCameraSettings CameraSettings, AActor* OriginalPointActor, ALevelSequenceActor*& OutActor);
 
 	void SetFlowEventReceiver(UFlowNode* FlowNode) { FlowEventReceiver = FlowNode; }
 
--- Source/Flow/Public/Nodes/World/FlowNode_PlayLevelSequence.h
+++ Source/Flow/Public/Nodes/World/FlowNode_PlayLevelSequence.h
@@ -43,6 +43,9 @@ class FLOW_API UFlowNode_PlayLevelSequence : public UFlowNode
 	
 	UPROPERTY(EditAnywhere, Category = "Sequence")
 	FLevelSequenceCameraSettings CameraSettings;
+
+	UPROPERTY(EditAnywhere, Category = "Sequence")
+	bool bUseGraphOwnerAsOriginalPointActor;
 	
 protected:
 	UPROPERTY()
@@ -50,6 +53,9 @@ protected:
 
 	UPROPERTY()
 	UFlowLevelSequencePlayer* SequencePlayer;
+
+	UPROPERTY()
+	AActor* GraphOwner;
 
 	// Play Rate set by the user in PlaybackSettings
 	float CachedPlayRate;
