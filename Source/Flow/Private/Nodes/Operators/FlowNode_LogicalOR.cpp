bab830aeceff2b4ddc0a7a9d9eb65980cd52aaf8
Mephiztopheles <m.ahrweiler@acinex.com>
2022-06-22 18:44:09 +0200

    The OR-Operator can now be executed once and can be reset if, it's only executed once

--- Source/Flow/Private/Nodes/Operators/FlowNode_LogicalOR.cpp
+++ Source/Flow/Private/Nodes/Operators/FlowNode_LogicalOR.cpp
@@ -2,6 +2,8 @@
 
 #include "Nodes/Operators/FlowNode_LogicalOR.h"
 
+static FName ResetName = TEXT("Reset");
+
 UFlowNode_LogicalOR::UFlowNode_LogicalOR(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
@@ -13,7 +15,42 @@ UFlowNode_LogicalOR::UFlowNode_LogicalOR(const FObjectInitializer& ObjectInitial
 	SetNumberedInputPins(0, 1);
 }
 
+void UFlowNode_LogicalOR::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	if (PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED(UFlowNode_LogicalOR, bOnce))
+	{
+		if (bOnce)
+		{
+			InputPins.EmplaceAt(0, ResetName);
+			OutputPins.Emplace(ResetName);
+		}
+		else
+		{
+			InputPins.Remove(ResetName);
+			OutputPins.Remove(ResetName);
+		}
+
+		OnReconstructionRequested.ExecuteIfBound();
+	}
+}
+
 void UFlowNode_LogicalOR::ExecuteInput(const FName& PinName)
 {
+	if (PinName == ResetName)
+	{
+		bCompleted = false;
+		TriggerOutput(ResetName, false);
+		return;
+	}
+
+	if (bOnce && bCompleted)
+	{
+		return;
+	}
+
+	bCompleted = true;
+
 	TriggerFirstOutput(true);
 }
