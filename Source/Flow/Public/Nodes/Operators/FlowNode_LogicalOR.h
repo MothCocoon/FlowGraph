2cdace4890c543e4d8ccd0c632a465e0d928c955
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2022-08-20 11:18:22 +0200

    Revert "The OR-Operator can now be executed once and can be reset if, it's only executed once (#109)"
    This reverts commit f138c4e350c9e461974aa1efb4deb6ad90a4c80a.

--- Source/Flow/Public/Nodes/Operators/FlowNode_LogicalOR.h
+++ Source/Flow/Public/Nodes/Operators/FlowNode_LogicalOR.h
@@ -13,21 +13,11 @@ UCLASS(NotBlueprintable, meta = (DisplayName = "OR"))
 class FLOW_API UFlowNode_LogicalOR final : public UFlowNode
 {
 	GENERATED_UCLASS_BODY()
+
 #if WITH_EDITOR
 	virtual bool CanUserAddInput() const override { return true; }
-
-	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
 #endif
 
-	
-
 protected:
-	/** If set to true, this node will only execute once until reset */
-	UPROPERTY(EditAnywhere)
-	bool bOnce = false;
-
-	UPROPERTY(SaveGame)
-	bool bCompleted = false;
-
 	virtual void ExecuteInput(const FName& PinName) override;
 };
