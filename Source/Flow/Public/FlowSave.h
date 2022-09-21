9a0c3cb6445eecc2d6aac2dfd57d476bb4aa5358
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2022-09-21 15:59:07 +0200

    compilation fixes

--- Source/Flow/Public/FlowSave.h
+++ Source/Flow/Public/FlowSave.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "CoreMinimal.h"
 #include "GameFramework/SaveGame.h"
 #include "Serialization/BufferArchive.h"
 #include "Serialization/ObjectAndNameAsStringProxyArchive.h"
