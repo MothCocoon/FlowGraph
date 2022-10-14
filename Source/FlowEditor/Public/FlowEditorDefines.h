5b3c965da852ac2a9ab059c755ca12470d7d77fa
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2022-10-14 22:01:33 +0200

    added ENABLE_FLOW_SEARCH define

--- Source/FlowEditor/Public/FlowEditorDefines.h
+++ Source/FlowEditor/Public/FlowEditorDefines.h
@@ -7,3 +7,9 @@
  * Set macro value to 1, if you made these changes to the engine: https://github.com/EpicGames/UnrealEngine/pull/9659
  */
 #define ENABLE_FLOW_DIFF 0
+
+/**
+* Documentation: https://github.com/MothCocoon/FlowGraph/wiki/Asset-Search
+* Set macro value to 1, if you made these changes to the engine: https://github.com/EpicGames/UnrealEngine/pull/9070
+ */
+#define ENABLE_FLOW_SEARCH 0
