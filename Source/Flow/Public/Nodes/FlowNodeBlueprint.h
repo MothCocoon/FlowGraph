db8c1594b9ddd65e4ca210ac1189610d8572b5de
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2022-10-16 21:09:03 +0200

    fixed Event Graph not created with new blueprint in 5.1

--- Source/Flow/Public/Nodes/FlowNodeBlueprint.h
+++ Source/Flow/Public/Nodes/FlowNodeBlueprint.h
@@ -9,7 +9,6 @@
 /**
  * A specialized blueprint class required for customizing Asset Type Actions
  */
-
 UCLASS(BlueprintType)
 class FLOW_API UFlowNodeBlueprint : public UBlueprint
 {
@@ -20,8 +19,6 @@ class FLOW_API UFlowNodeBlueprint : public UBlueprint
 	virtual bool SupportedByDefaultBlueprintFactory() const override { return false; }
 
 	virtual bool SupportsDelegates() const override { return false; }
-	virtual bool SupportsEventGraphs() const override { return false; }
-	virtual bool SupportsAnimLayers() const override { return false; }
 	// --
 #endif
 };
