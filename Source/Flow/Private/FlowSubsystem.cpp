8b2d3b4e4d3bb4614c7b6f6cd55bb9c751e43c4a
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2022-10-18 20:01:26 +0200

    PR #120 cleanup: removal of subsystem part, renamed event to Activate

--- Source/Flow/Private/FlowSubsystem.cpp
+++ Source/Flow/Private/FlowSubsystem.cpp
@@ -48,14 +48,6 @@ void UFlowSubsystem::Deinitialize()
 	AbortActiveFlows();
 }
 
-void UFlowSubsystem::OnNodeInputTriggered(const UFlowNode* node, const bool bWasActive)
-{
-}
-
-void UFlowSubsystem::OnNodeOutputTriggered(const UFlowNode* node, const bool bFinish)
-{
-}
-
 void UFlowSubsystem::AbortActiveFlows()
 {
 	if (InstancedTemplates.Num() > 0)
