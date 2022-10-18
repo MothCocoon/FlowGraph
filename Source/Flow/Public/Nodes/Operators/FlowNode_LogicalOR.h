347bb4a6a56a57e684b777071d4c6d7a38646450
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2022-10-19 00:30:14 +0200

    Reworked version of #109 - possible to disable infinite execution of the OR node
    Now OR node executes output only once, by default. Added logic to enable/disable the `Logical OR` node.
    
    This can be a breaking change if you somewhere relied on an infinitely working OR node. You can fix this by changing the `Execution Limit` value to 0.

--- Source/Flow/Public/Nodes/Operators/FlowNode_LogicalOR.h
+++ Source/Flow/Public/Nodes/Operators/FlowNode_LogicalOR.h
@@ -14,10 +14,27 @@ class FLOW_API UFlowNode_LogicalOR final : public UFlowNode
 {
 	GENERATED_UCLASS_BODY()
 
+protected:
+	UPROPERTY(EditAnywhere, Category = "Lifetime", SaveGame)
+	bool bEnabled;
+	
+	// This node will become Blocked (not executed any more), if Execution Limit > 0 and Execution Count reaches this limit
+	// Set this to zero, if you'd like fire output indefinitely
+	UPROPERTY(EditAnywhere, Category = "Lifetime", meta = (ClampMin = 0))
+	int32 ExecutionLimit;
+
+	// This node will become Blocked (not executed any more), if Execution Limit > 0 and Execution Count reaches this limit
+	UPROPERTY(VisibleAnywhere, Category = "Lifetime", SaveGame)
+	int32 ExecutionCount;
+
 #if WITH_EDITOR
+public:
 	virtual bool CanUserAddInput() const override { return true; }
 #endif
 
 protected:
 	virtual void ExecuteInput(const FName& PinName) override;
+	virtual void Cleanup() override;
+
+	void ResetCounter();
 };
