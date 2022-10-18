06d8996ee813df21e64995595d43d33f646b79be
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2022-10-18 21:46:18 +0200

    ANY_PACKAGE fix: removed redundant search for static class

--- Source/FlowEditor/FlowEditor.Build.cs
+++ Source/FlowEditor/FlowEditor.Build.cs
@@ -35,6 +35,7 @@ public class FlowEditor : ModuleRules
             "Kismet",
             "KismetWidgets",
             "LevelEditor",
+            "LevelSequence",
             "MovieScene",
             "MovieSceneTracks",
             "MovieSceneTools",
