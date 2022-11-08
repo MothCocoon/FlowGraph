d41d16502631a9cdef3a9dc629e25e76938ea1fa
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2022-11-08 23:08:58 +0100

    cosmetic cleanup

--- Source/Flow/Private/Nodes/Route/FlowNode_Timer.cpp
+++ Source/Flow/Private/Nodes/Route/FlowNode_Timer.cpp
@@ -7,11 +7,11 @@
 
 UFlowNode_Timer::UFlowNode_Timer(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
-	  , CompletionTime(1.0f)
-	  , StepTime(0.0f)
-	  , SumOfSteps(0.0f)
-	  , RemainingCompletionTime(0.0f)
-	  , RemainingStepTime(0.0f)
+	, CompletionTime(1.0f)
+	, StepTime(0.0f)
+	, SumOfSteps(0.0f)
+	, RemainingCompletionTime(0.0f)
+	, RemainingStepTime(0.0f)
 {
 #if WITH_EDITOR
 	Category = TEXT("Route");
@@ -138,17 +138,18 @@ void UFlowNode_Timer::OnSave_Implementation()
 
 void UFlowNode_Timer::OnLoad_Implementation()
 {
-	if (RemainingStepTime > 0.0f)
+	if (RemainingStepTime > 0.0f || RemainingCompletionTime > 0.0f)
 	{
-		GetWorld()->GetTimerManager().SetTimer(StepTimerHandle, this, &UFlowNode_Timer::OnStep, StepTime, true,
-		                                       RemainingStepTime);
+		if (RemainingStepTime > 0.0f)
+		{
+			GetWorld()->GetTimerManager().SetTimer(StepTimerHandle, this, &UFlowNode_Timer::OnStep, StepTime, true, RemainingStepTime);
+		}
+
+		GetWorld()->GetTimerManager().SetTimer(CompletionTimerHandle, this, &UFlowNode_Timer::OnCompletion, RemainingCompletionTime, false);
+
+		RemainingStepTime = 0.0f;
+		RemainingCompletionTime = 0.0f;
 	}
-
-	GetWorld()->GetTimerManager().SetTimer(CompletionTimerHandle, this, &UFlowNode_Timer::OnCompletion,
-	                                       RemainingCompletionTime, false);
-
-	RemainingStepTime = 0.0f;
-	RemainingCompletionTime = 0.0f;
 }
 
 #if WITH_EDITOR
@@ -177,8 +178,7 @@ FString UFlowNode_Timer::GetStatusString() const
 
 	if (CompletionTimerHandle.IsValid() && GetWorld())
 	{
-		return TEXT("Progress: ") + GetProgressAsString(
-			GetWorld()->GetTimerManager().GetTimerElapsed(CompletionTimerHandle));
+		return TEXT("Progress: ") + GetProgressAsString(GetWorld()->GetTimerManager().GetTimerElapsed(CompletionTimerHandle));
 	}
 
 	return FString();
