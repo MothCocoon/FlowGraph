7ca21198df48e261b53d2a9cb1705d1f3295aaa6
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2022-12-30 17:13:49 +0100

    Asset Search: added option to run search on single asset, with Search Browser opened as asset editor tab
    Requires engine modification: EpicGames/UnrealEngine#9943
    
    docs: https://github.com/MothCocoon/FlowGraph/wiki/Asset-Search

--- Source/FlowEditor/Private/Asset/FlowAssetToolbar.cpp
+++ Source/FlowEditor/Private/Asset/FlowAssetToolbar.cpp
@@ -196,16 +196,14 @@ void FFlowAssetToolbar::BuildAssetToolbar(UToolMenu* ToolbarMenu) const
 	FToolMenuSection& Section = ToolbarMenu->AddSection("Editing");
 	Section.InsertPosition = FToolMenuInsert("Asset", EToolMenuInsertType::After);
 
+	// add buttons
 	Section.AddEntry(FToolMenuEntry::InitToolBarButton(FFlowToolbarCommands::Get().RefreshAsset));
+#if ENABLE_SEARCH_IN_ASSET_EDITOR
+	Section.AddEntry(FToolMenuEntry::InitToolBarButton(FFlowToolbarCommands::Get().SearchInAsset));
+#endif	
 
-	/**
-	 * Documentation: https://github.com/MothCocoon/FlowGraph/wiki/Visual-Diff
-	 * Set macro value to 1, if you made these changes to the engine: https://github.com/EpicGames/UnrealEngine/pull/9659
-	 */
-#if ENABLE_FLOW_DIFF
-	FToolMenuSection& DiffSection = ToolbarMenu->AddSection("SourceControl");
-	DiffSection.InsertPosition = FToolMenuInsert("Asset", EToolMenuInsertType::After);
-	DiffSection.AddDynamicEntry("SourceControlCommands", FNewToolMenuSectionDelegate::CreateLambda([this](FToolMenuSection& InSection)
+	// Visual Diff: menu to choose asset revision compared with the current one 
+	Section.AddDynamicEntry("SourceControlCommands", FNewToolMenuSectionDelegate::CreateLambda([this](FToolMenuSection& InSection)
 	{
 		InSection.InsertPosition = FToolMenuInsert();
 		FToolMenuEntry DiffEntry = FToolMenuEntry::InitComboButton(
