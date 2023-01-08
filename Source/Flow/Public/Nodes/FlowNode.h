f07b94e5d599df0b513e3fb717ec58017701a91a
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2023-01-09 00:56:15 +0100

    import utils improvements
    - importing Comment nodes
    - importing BaseAsyncTask nodes (i.e. gameplay tasks)
    - added UFlowNode::PostImport()
    - supporting nodes with multiple outputs

--- Source/Flow/Public/Nodes/FlowNode.h
+++ Source/Flow/Public/Nodes/FlowNode.h
@@ -78,6 +78,9 @@ public:
 	virtual void FixNode(UEdGraphNode* NewGraphNode);
 
 	virtual EDataValidationResult ValidateNode() { return EDataValidationResult::NotValidated; }
+
+	// used when import graph from another asset
+	virtual void PostImport() {}
 #endif
 
 	UEdGraphNode* GetGraphNode() const { return GraphNode; }
