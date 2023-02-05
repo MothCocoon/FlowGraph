1f50d2934bff7e2014d855d5cc037c570ee713d2
Jani Hartikainen <jani.hartikainen@gmail.com>
2023-02-05 12:41:06 +0200

    Allow Sequence nodes to execute new connections

--- Source/Flow/Private/Nodes/Route/FlowNode_ExecutionSequence.cpp
+++ Source/Flow/Private/Nodes/Route/FlowNode_ExecutionSequence.cpp
@@ -16,10 +16,48 @@ UFlowNode_ExecutionSequence::UFlowNode_ExecutionSequence(const FObjectInitialize
 
 void UFlowNode_ExecutionSequence::ExecuteInput(const FName& PinName)
 {
+	if(bSavePinExecutionState)
+	{
+		ExecuteNewConnections();
+		return;
+	}
+	
 	for (const FFlowPin& Output : OutputPins)
 	{
 		TriggerOutput(Output.PinName, false);
 	}
 
 	Finish();
+}
+
+#if WITH_EDITOR
+FString UFlowNode_ExecutionSequence::GetNodeDescription() const
+{
+	if(bSavePinExecutionState)
+	{
+		return TEXT("Saves pin execution state");
+	}
+	
+	return Super::GetNodeDescription();
+}
+#endif
+
+void UFlowNode_ExecutionSequence::OnLoad_Implementation()
+{
+	ExecuteNewConnections();
+}
+
+void UFlowNode_ExecutionSequence::ExecuteNewConnections()
+{
+	for (const FFlowPin& Output : OutputPins)
+	{
+		const FConnectedPin& Connection = GetConnection(Output.PinName);
+		if(ExecutedConnections.Contains(Connection.NodeGuid))
+		{
+			continue;
+		}
+
+		TriggerOutput(Output.PinName, false);
+		ExecutedConnections.Emplace(Connection.NodeGuid);
+	}
 }
