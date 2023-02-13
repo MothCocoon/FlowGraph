441c471eab18b7986d816f780b56f9787638c4f9
ryanjon2040 <mindfreak2040@gmail.com>
2023-02-13 13:00:42 +0530

    Add delay until next tick node
    This node will run its output (if supported) in next frame

---
+++ Source/Flow/Private/Nodes/Route/FlowNode_DelayUntilNextTick.cpp
@@ -0,0 +1,34 @@
+﻿// Copyright https://github.com/MothCocoon/FlowGraph/graphs/contributors
+
+#include "Nodes/Route/FlowNode_DelayUntilNextTick.h"
+
+UFlowNode_DelayUntilNextTick::UFlowNode_DelayUntilNextTick()
+{
+#if WITH_EDITOR
+	Category = TEXT("Route");
+	NodeStyle = EFlowNodeStyle::Latent;
+#endif
+	
+	OutputPins.Empty();
+	OutputPins.Add(FFlowPin(TEXT("Completed"), "Executed in next frame."));
+	OutputPins.Add(FFlowPin(TEXT("Skipped"), "Executed if GetWorld is null and this pin will run in same frame."));
+}
+
+void UFlowNode_DelayUntilNextTick::ExecuteInput(const FName& PinName)
+{
+	const UWorld* World = GetWorld();
+	if (IsValid(World))
+	{
+		World->GetTimerManager().SetTimerForNextTick(FTimerDelegate::CreateUObject(this, &ThisClass::OnCompletion));
+	}
+	else
+	{
+		LogError(TEXT("No valid world for delay until next tick node"));
+		TriggerOutput(TEXT("Skipped"), true);
+	}
+}
+
+void UFlowNode_DelayUntilNextTick::OnCompletion()
+{
+	TriggerOutput(TEXT("Completed"), true);
+}
---
+++ Source/Flow/Public/Nodes/Route/FlowNode_DelayUntilNextTick.h
@@ -0,0 +1,29 @@
+﻿// Copyright https://github.com/MothCocoon/FlowGraph/graphs/contributors
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Nodes/FlowNode.h"
+#include "FlowNode_DelayUntilNextTick.generated.h"
+
+/**
+ * Triggers output next frame
+ */
+UCLASS(NotBlueprintable, meta = (DisplayName = "Delay until next tick", Keywords = "frame"))
+class FLOW_API UFlowNode_DelayUntilNextTick : public UFlowNode
+{
+	GENERATED_BODY()
+
+public:
+
+	UFlowNode_DelayUntilNextTick();
+
+protected:
+
+	virtual void ExecuteInput(const FName& PinName) override;
+
+private:
+
+	UFUNCTION()
+	void OnCompletion();
+};
