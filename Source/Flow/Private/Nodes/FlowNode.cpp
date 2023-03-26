8d243520b56b75a78b78eb7abd5756640268a704
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2023-03-26 12:32:47 +0200

    formatting fix

--- Source/Flow/Private/Nodes/FlowNode.cpp
+++ Source/Flow/Private/Nodes/FlowNode.cpp
@@ -578,16 +578,16 @@ void UFlowNode::LoadInstance(const FFlowNodeSaveData& NodeRecord)
 	{
 		case EFlowSignalMode::Enabled:
 			OnLoad();
-		break;
+			break;
 		case EFlowSignalMode::Disabled:
 			// designer doesn't want to execute this node's logic at all, so we kill it
 			LogNote(TEXT("Signal disabled while loading Flow Node from SaveGame"));
-		Finish();
-		break;
+			Finish();
+			break;
 		case EFlowSignalMode::PassThrough:
 			LogNote(TEXT("Signal pass-through on loading Flow Node from SaveGame"));
-		OnPassThrough();
-		break;
+			OnPassThrough();
+			break;
 		default: ;
 	}
 }
