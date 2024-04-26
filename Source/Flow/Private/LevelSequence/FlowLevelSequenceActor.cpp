87042c06239d123d3065072ce4f191b11d667e64
MaksymKapelianovych <m.kapelianovych@gmail.com>
2024-04-26 11:28:56 +0300

    Fix crash with SequencePlayer by adding engine version check

--- Source/Flow/Private/LevelSequence/FlowLevelSequenceActor.cpp
+++ Source/Flow/Private/LevelSequence/FlowLevelSequenceActor.cpp
@@ -22,7 +22,11 @@ void AFlowLevelSequenceActor::GetLifetimeReplicatedProps(TArray<FLifetimePropert
 void AFlowLevelSequenceActor::SetPlaybackSettings(FMovieSceneSequencePlaybackSettings NewPlaybackSettings)
 {
 	PlaybackSettings = NewPlaybackSettings;
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION > 3
 	GetSequencePlayer()->SetPlaybackSettings(PlaybackSettings);
+#else
+	SequencePlayer->SetPlaybackSettings(PlaybackSettings);
+#endif
 }
 
 void AFlowLevelSequenceActor::SetReplicatedLevelSequenceAsset(ULevelSequence* Asset)
