5cd97a8aca7e3fdc5b081c0eb3646f77ff564640
Mickaël <mfourgeaud@gmail.com>
2025-04-03 11:12:15 +0300

    Use weak pointer instead of raw pointer in log lambda

--- Source/Flow/Private/FlowComponent.cpp
+++ Source/Flow/Private/FlowComponent.cpp
@@ -273,11 +273,14 @@ void UFlowComponent::LogError(FString Message, const EFlowOnScreenMessageType On
 		{
 			if (UViewportStatsSubsystem* StatsSubsystem = GetWorld()->GetSubsystem<UViewportStatsSubsystem>())
 			{
-				StatsSubsystem->AddDisplayDelegate([this, Message](FText& OutText, FLinearColor& OutColor)
+				// Create a weak pointer to this
+				TWeakObjectPtr WeakThis(this);
+
+				StatsSubsystem->AddDisplayDelegate([WeakThis, Message](FText& OutText, FLinearColor& OutColor)
 				{
 					OutText = FText::FromString(Message);
 					OutColor = FLinearColor::Red;
-					return IsValid(this);
+					return WeakThis.IsValid();
 				});
 			}
 		}
--- Source/Flow/Private/Nodes/FlowNodeBase.cpp
+++ Source/Flow/Private/Nodes/FlowNodeBase.cpp
@@ -815,12 +815,15 @@ void UFlowNodeBase::LogError(FString Message, const EFlowOnScreenMessageType OnS
 			{
 				if (UViewportStatsSubsystem* StatsSubsystem = GetWorld()->GetSubsystem<UViewportStatsSubsystem>())
 				{
-					StatsSubsystem->AddDisplayDelegate([this, Message](FText& OutText, FLinearColor& OutColor)
+				    // Create a weak pointer to this
+				    TWeakObjectPtr WeakThis(this);
+				    
+					StatsSubsystem->AddDisplayDelegate([WeakThis, Message](FText& OutText, FLinearColor& OutColor)
 					{
 						OutText = FText::FromString(Message);
 						OutColor = FLinearColor::Red;
 
-						return IsValid(this) && GetFlowNodeSelfOrOwner()->GetActivationState() != EFlowNodeState::NeverActivated;
+						return WeakThis.IsValid() && WeakThis->GetFlowNodeSelfOrOwner()->GetActivationState() != EFlowNodeState::NeverActivated;
 					});
 				}
 			}
