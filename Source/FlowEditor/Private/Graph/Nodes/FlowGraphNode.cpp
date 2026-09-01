61535184aee1e4878b444cacbeafe35508c7850f
pyi <pyi@cyanide-studio.com>
2026-09-01 10:02:50 +0200

    Changed PIE context guard during ForcePinActivation
    We previously set the PlayInEditorID so that forced ExecuteInput calls are executed with correct WorldContext, but this is not enough in case BP functions are executed.
    
    To ensure soft pointers will resolve correctly, we need to override the WorldContext altogether.

--- Source/FlowEditor/Private/Graph/Nodes/FlowGraphNode.cpp
+++ Source/FlowEditor/Private/Graph/Nodes/FlowGraphNode.cpp
@@ -1184,24 +1184,19 @@ void UFlowGraphNode::ForcePinActivation(const FEdGraphPinReference PinReference)
 		return;
 	}
 
-	FWorldContext* WorldContext = GEngine->GetWorldContextFromWorld(InspectedNodeInstance->GetWorld());
-	if (WorldContext && WorldContext->PIEInstance != INDEX_NONE)
-	{
-		FTemporaryPlayInEditorIDOverride PIEGuard(WorldContext->PIEInstance);
+	FScopedConditionalWorldSwitcher WorldSwitcher(InspectedNodeInstance->GetWorld());
 
-		if (const UEdGraphPin* FoundPin = PinReference.Get())
+	if (const UEdGraphPin* FoundPin = PinReference.Get())
+	{
+		switch (FoundPin->Direction)
 		{
-			switch (FoundPin->Direction)
-			{
 			case EGPD_Input:
 				InspectedNodeInstance->TriggerInput(FoundPin->PinName, EFlowPinActivationType::Forced);
 				break;
 			case EGPD_Output:
 				InspectedNodeInstance->TriggerOutput(FoundPin->PinName, false, EFlowPinActivationType::Forced);
 				break;
-			default:
-				break;
-			}
+			default: ;
 		}
 	}
 }
